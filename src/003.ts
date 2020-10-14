de9cef9efb2b1f352fa63fde055e5e61eb417f78
Sean Barnard <sean.barnard.94@hotmail.com>
2020-10-14 23:12:28 +0100

    added boilerplate for problem 3

---
+++ src/003.ts
@@ -0,0 +1,17 @@
+// PROBLEM
+// # Problem 3
+// ## Largest prime factor
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+// SOLUTION
+const getPrimeFactors = (input: number) => {
+  // implement recursive logic here
+};
+
+// TESTS
+describe("Problem 3 - Largest prime factor", () => {
+  it("test case", () => {
+    expect(true).toBeTruthy();
+  });
+});
